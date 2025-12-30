d6b32560395f75a59df5e0777eebd46d87781316
thodkarik <thod.karik@gmail.com>
2025-12-30 17:54:08 +0200

    feat: add class and functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+
+import ClassComponent from "./components/ClassComponent.tsx";
+import FunctionalComponent from "./components/FunctionalComponent.tsx";
+import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 
 function App() {
 
   return (
     <>
-        <ViteIntro />
+        {/*<ViteIntro />*/}
+        <ClassComponent />
+        <FunctionalComponent />
+        <ArrowFunctionalComponent/>
     </>
   )
 }
