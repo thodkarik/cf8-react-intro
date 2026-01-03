2c22de8b804eb7c9be308e20c68f428c21218b8b
thodkarik <thod.karik@gmail.com>
2026-01-03 23:37:01 +0200

    feat: add CounterAdvanced component

---
+++ src/components/CounterAdvanced.tsx
@@ -0,0 +1,72 @@
+import {useState} from "react";
+import CounterButton from "./CounterButton.tsx";
+
+type CounterState = {
+    count: number;
+    lastAction: string;
+    time: string;
+}
+
+const CounterAdvanced = () => {
+
+    const [state, setState] = useState<CounterState>({
+        count: 0,
+        lastAction: "",
+        time: "",
+    });
+
+    const getCurrentTime = () => new Date().toLocaleTimeString();
+
+    const increaseCount = () => {
+        setState({
+            count: state.count + 1,
+            lastAction: "Increase",
+            time: getCurrentTime(),
+        });
+    }
+    const decreaseCount = () => {
+        if (state.count > 0) {
+            setState({
+                count: state.count - 1,
+                lastAction: "Decrease",
+                time: getCurrentTime(),
+            });
+        }
+    }
+    const resetCount = () => {
+        if (state.count > 0) {
+            setState({
+                count: 0,
+                lastAction: "Reset",
+                time: getCurrentTime(),
+            });
+        }
+    }
+    return (
+        <>
+            <h1 className="text-center text-2xl mt-12">Count is <strong>{state.count}</strong></h1>
+            <div className="text-center space-x-4 mt-8">
+                <CounterButton
+                    onClick={increaseCount}
+                    label="Increase"
+                />
+                <CounterButton
+                    onClick={decreaseCount}
+                    label="Decrease"
+                    disabled={state.count === 0}
+                />
+                <CounterButton
+                    onClick={resetCount}
+                    label="Reset"
+                    disabled={state.count === 0}
+                    addClasses="bg-cf-dark-red disabled:bg-red-500"
+                />
+            </div>
+            <p className="text-center text-cf-gray pt-8">
+                Last Change: <strong>{state.lastAction || "-"}</strong> at <strong>{state.time || "-"}</strong>
+            </p>
+        </>
+    )
+}
+
+export default CounterAdvanced;
